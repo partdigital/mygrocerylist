2258811a0198234e3c8b093362af60a267c1922d
jbolduc <jbolduc@genuineinteractive.com>
2015-05-25 16:03:15 -0400

    Day 6: Query Strings and MySQL

--- item.php
+++ item.php
@@ -5,20 +5,65 @@
         // redirect to login.php
         header('location:login.php'); 
     }   
+
+    // If query string is set 
+    if (!empty($_GET['id'])) {
+
+        // Select that row from the database 
+        $sql = 'SELECT `name`,`qty` FROM `items` WHERE `id` = "' . $_GET['id'] . '"'; 
+        $result = mysqli_query($db, $sql); 
+
+        $data = mysqli_fetch_assoc($result); 
+    }
+
+
+    // Form submit handler 
+    if (!empty($_POST)) {
+        
+        foreach($_POST as $key => $value) {
+            // Sanitize input
+            $value = strip_tags($value); 
+            
+            // Save sanitized post values into form array 
+            // This array is used to populate the form after form submission. 
+            $_POST[$key] = $value; 
+        }
+        
+        // If query string is set then update record 
+        if (!empty($_GET['id'])) {
+            $sql = 'UPDATE `items` SET 
+            `name` = "' . $_POST['name'] . '",  
+            `qty` = "' . $_POST['qty'] . '"
+            WHERE id = "' . $_GET['id']. '"';  
+            
+            // Run query.
+            mysqli_query($db, $sql); 
+            header('location:list.php'); 
+            
+        // If ID is not set then insert record
+		} else {
+            $sql = 'INSERT INTO `items` (`name`, `qty`) VALUES ("' . $_POST['name'] . '", "' . $_POST['qty'].'")'; 
+            
+            // Run query.
+            mysqli_query($db, $sql); 
+            header('location:list.php'); 
+        }
+        
+    }
 ?>
 <?php include 'includes/header.php'; ?>
 <?php include 'includes/menu.php'; ?>
     <div id = "content">
         <div class="content">
             <h1>Grocery Item</h1>   
-            <form action = "">
+            <form action = "" method = "POST">
                 <div>
                     <label for = "name">Name:</label>
-                    <input type = "text" name ="name" value = "" size = "45" />
+                    <input type = "text" name ="name" value = "<?php if (!empty($data['name'])) print $data['name']; ?>" size = "45" />
                 </div>
                 <div>
                     <label for = "qty">Quanity</label>
-                    <input type = "text" name="qty" value = "" size = "45" />
+                    <input type = "text" name="qty" value = "<?php if (!empty($data['qty'])) print $data['qty']; ?>" size = "45" />
                 </div>
                 <input type = "submit" value = "Save Item">
             </form>
--- list.php
+++ list.php
@@ -5,24 +5,52 @@
         // redirect to login.php
         header('location:login.php'); 
     }   
+
+    // Fetching groceries
+    // Query statement 
+    $sql = "SELECT `id`, `name`, `qty` FROM `items`";
+
+    // Run query statement 
+    $result = mysqli_query($db, $sql);  
 ?>
 <?php include 'includes/header.php'; ?>
 <?php include 'includes/menu.php'; ?>
     <div id = "content">
         <div class="content">
             <h1>Grocery List</h1>   
-            <table>
-                <tr>
-                    <td>Carrots</td>
-                    <td>1 bag</td>
-                    <td>Edit / Delete</td>
-                </tr>
-                <tr>
-                    <td>Bread</td>
-                    <td>1 loaf </td>
-                    <td>Edit / Delete</td>
-                </tr>
-            </table>
+<?php 
+// Fetch all data. 
+$data = mysqli_fetch_all($result, MYSQLI_ASSOC); 
+
+if(!empty($data)) { 
+    // Start Table tag 
+    print '<table>';  
+    
+    // Loop through each row in the database 
+    // Store each row as an associative array 
+    foreach ($data as $row) {
+        // Opening row tag 
+        print '<tr>'; 
+        
+        // Output the name
+        print '<td>' . $row['name'] . '</td>';
+        
+        // Output quantity 
+        print '<td>' . $row['qty'] . '</td>'; 
+       
+        // Output Edit/Delete links with query strings. 
+        print '<td>';
+        print '<a href = "item.php?id=' . $row['id'] . '">Edit</a> | '; 
+        print '<a href = "">Delete</a>'; 
+        print '</td>'; 
+        // Closing row tag 
+        print '</tr>'; 
+            
+    }
+    // Closing table tag 
+    print '</table>'; 
+}
+?> 
         </div>
     </div>
 <?php include 'includes/footer.php'; ?>

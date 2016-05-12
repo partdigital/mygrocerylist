8217328a297aa84d0a3a3d9770b1196ad49477a3
jbolduc <jbolduc@genuineinteractive.com>
2016-05-11 20:24:59 -0400

    temp

--- bootstrap.php
+++ bootstrap.php
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
-<?php 
-// Config
-=======
 <?php
 // Start session 
 session_start(); 
 
-// Config
->>>>>>> Day 4: Sessions and menu loop
->>>>>>> d310f60... Day 4: Sessions and menu loop
 include 'config.php'; 
